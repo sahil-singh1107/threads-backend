6019df10bd119c886341d9cda56499e1f14f5e7a
sahil-singh1107 <sahilpython@gmail.com>
2024-09-13 17:50:47 +0530

    code refactor

---
+++ src/graphql/user/index.ts
@@ -0,0 +1,5 @@
+import {typeDefs} from "./typedef"
+import {queries} from './queries'
+import {mutations} from "./mutations"
+import {resolvers} from "./resolvers"
+export const User = {typeDefs, queries, resolvers, mutations}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,18 @@
 import express from "express"
-import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from "@apollo/server/express4";
+import createApolloGraphqlServer from "./graphql"
+
 
 async function  init () {
     const app = express()
     const PORT = Number(process.env.PORT) || 8000;
-
     app.use(express.json())
-
-// create graphql server
-
-    const gqlServer = new ApolloServer({
-        typeDefs: `
-            type Query {
-                hello: String
-                say(name: String): String
-            }
-        `,
-        resolvers: {
-            Query: {
-                hello: () => `hey there i am graphql server`,
-                say: (_,{name} : {name: String}) => `${name}`,
-            }
-        }
-    })
-
-    await gqlServer.start()
-//start the server
 
     app.get("/", (req, res) => {
         res.json({message: "server is up and running"})
     })
 
-    app.use("/graphql", expressMiddleware(gqlServer));
+    app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
 
     app.listen(PORT, () => {
         console.log("Server is up and running on port: " + PORT);
